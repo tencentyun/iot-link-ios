bb471575706314c599a43efcf6af8b389dde3091
ccharlesren <ccharlesren@tencent.com>
2020-08-05 11:56:56 +0800

    替换隐私协议和用户协议URL链接
    http: //tapd.oa.com/20393192/prong/stories/view/1020393192859025221
    
    Change-Id: Ia52c78e114d27eb9552d7b5beadcb55af30fb6fd

--- Source/LinkSDK/QCAPISets/Other/TIoTCoreRequestAction.m
+++ Source/LinkSDK/QCAPISets/Other/TIoTCoreRequestAction.m
@@ -117,4 +117,9 @@ NSString *const AppRemoveUserShareDevice = @"AppRemoveUserShareDevice";//删除�
 //===============h5
 NSString *const H5HelpCenter = @"help-center";//帮助中心
 
+//===============用户协议、隐私协议链接
+
+NSString *const ServiceProtocolURl = @"https://iot.cloud.tencent.com/explorer-h5/about-policy/#?type=service";//用户协议
+NSString *const PrivacyProtocolURL = @"https://iot.cloud.tencent.com/explorer-h5/about-policy/#?type=privacy";//隐私协议
+NSString *const CancelAccountURL = @"https://iot.cloud.tencent.com/explorer-h5/about-policy/#?type=cancel ";//账号注销协议
 @end
