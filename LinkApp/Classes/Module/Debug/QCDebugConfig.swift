520cacb8ca591836a8f5f57b9d5f241131f65fc0
eagleychen(陈鹰) <tonychanchen@gmail.com>
2020-08-10 21:13:25 +0800

    修改uin为全局变量，方便修改
    Change-Id: I337623c2cbb2f822320fb3aa6a8b1a99f741d216

---
+++ LinkApp/Classes/Module/Debug/QCDebugConfig.swift
@@ -0,0 +1,15 @@
+//
+//  QCDebugConfig.swift
+//  LinkApp
+//
+//  Created by eagleychen on 2020/8/10.
+//  Copyright © 2020 Tencent. All rights reserved.
+//
+
+import Foundation
+
+@objcMembers
+class QCDebugConfig: NSObject {
+
+    public static let GlobalDebugUin = "help_center_h5_api"
+}
