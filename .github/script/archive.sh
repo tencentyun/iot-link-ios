5475947d67d4fc2a1c1c5e5d4e35e4d37a9c3737
eagleychen(陈鹰) <tonychanchen@gmail.com>
2020-08-17 11:50:59 +0800

    调试version

--- .github/script/archive.sh
+++ .github/script/archive.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
 
 #bin/bsah - l
-
+rb=$(git rev-parse --abbrev-ref HEAD)
 rc=$(git rev-parse --short HEAD)
-#echo $rc
+rt=$(git describe --tags `git rev-list --tags --max-count=1`)
+
+echo $rb
+echo $rc
+echo $rt
+
 if [ $1 == 'Debug' ]; then
-    sed -i "" '/LinkAPP_VERSION/ s/$/.'$rc'/' LinkApp/Supporting\ Files/LinkAppCommon.xcconfig
+#    sed -i "" '/LinkAPP_VERSION/ s/$/.'$rc'/' LinkApp/Supporting\ Files/LinkAppCommon.xcconfig
+#    sed "s/LinkAPP_VERSION.*/LinkAPP_VERSION = xxx111xxx/g" LinkApp/Supporting\ Files/LinkAppCommon.xcconfig
+    sed -i "" "s/LinkAPP_VERSION.*/LinkAPP_VERSION = '$rb'+git.'$rc'/g" LinkApp/Supporting\ Files/LinkAppCommon.xcconfig
+else
+    sed -i "" "s/LinkAPP_VERSION.*/LinkAPP_VERSION = '$rt'+git.'$rc'/g" LinkApp/Supporting\ Files/LinkAppCommon.xcconfig
 fi
+
+cat LinkApp/Supporting\ Files/LinkAppCommon.xcconfig
 
 #rm -rf Podfile.lock
 #/usr/local/bin/pod install --verbose --no-repo-update
