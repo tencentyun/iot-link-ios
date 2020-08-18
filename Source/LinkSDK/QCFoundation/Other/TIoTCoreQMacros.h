6994d2289e3d7da7d823199ff7917f6032e50a35
v_vweisun(孙伟) <532125505@qq.com>
2020-08-18 20:11:35 +0800

    NSObject+ro去重
    Change-Id: I3b68cd97dc0b337cce8b00a482ee4fda38c4968d

--- Source/LinkSDK/QCFoundation/Other/TIoTCoreQMacros.h
+++ Source/LinkSDK/QCFoundation/Other/TIoTCoreQMacros.h
@@ -20,6 +20,23 @@
 
 #define kScreenWidth [UIScreen mainScreen].bounds.size.width
 #define kScreenHeight [UIScreen mainScreen].bounds.size.height
+
+#define Is_iphone (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)
+#define Is_iphoneX kScreenWidth >=375.0f && kScreenHeight >=812.0f && Is_iphone
+
+/*状态栏高度*/
+#define kStatusBarHeight (CGFloat)(Is_iphoneX?(44.0):(20.0))
+/*导航栏高度*/
+#define kNavBarHeight (44)
+/*状态栏和导航栏总高度*/
+#define kNavBarAndStatusBarHeight (CGFloat)(Is_iphoneX?(88.0):(64.0))
+/*TabBar高度*/
+#define kTabBarHeight (CGFloat)(Is_iphoneX?(49.0 + 34.0):(49.0))
+/*顶部安全区域远离高度*/
+#define kTopBarSafeHeight (CGFloat)(Is_iphoneX?(44.0):(0))
+/*底部安全区域远离高度*/
+#define kBottomSafeHeight (CGFloat)(Is_iphoneX?(34.0):(0))
+
 #import "UIFont+TIoTFont.h"
 #import "TIoTCoreWMacros.h"
 
