331f600535d308f6a0527d6777600d578d78143e
v_vweisun(孙伟) <532125505@qq.com>
2020-08-26 09:37:22 +0800

    修改获取网关的方法。
    Change-Id: I94fa8370aecf9a3f3e4f31f351dc338ed162905b

--- Source/LinkSDK/QCFoundation/Other/NSString+Extension.m
+++ Source/LinkSDK/QCFoundation/Other/NSString+Extension.m
@@ -156,52 +156,18 @@
     return hash;
 }
 
-+ (NSString *)getGateway
-{
-    NSString *address = @"error";
-    struct ifaddrs *interfaces = NULL;
-    struct ifaddrs *temp_addr = NULL;
-    int success = 0;
-    
-    // retrieve the current interfaces - returns 0 on success
-    success = getifaddrs(&interfaces);
-    if (success == 0) {
-        temp_addr = interfaces;
-        while(temp_addr != NULL) {
-            if(temp_addr->ifa_addr->sa_family == AF_INET) {
-                // Check if interface is en0 which is the wifi connection on the iPhone
-                if([[NSString stringWithUTF8String:temp_addr->ifa_name] isEqualToString:@"en0"])
-                {
-                    address = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_addr)->sin_addr)];
-                    NSLog(@"本机地址：%@",address);
-                    
-                    //routerIP----192.168.1.255 广播地址
-                    NSLog(@"广播地址：%@",[NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_dstaddr)->sin_addr)]);
-                    
-                    //--192.168.1.106 本机地址
-                    NSLog(@"本机地址：%@",[NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_addr)->sin_addr)]);
-                    
-                    //--255.255.255.0 子网掩码地址
-                    NSLog(@"子网掩码地址：%@",[NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_netmask)->sin_addr)]);
-                    
-                    //--en0 接口
-                    //  en0       Ethernet II    protocal interface
-                    //  et0       802.3             protocal interface
-                    //  ent0      Hardware device interface
-                    NSLog(@"接口名：%@",[NSString stringWithUTF8String:temp_addr->ifa_name]);
-                }
-            }
-            temp_addr = temp_addr->ifa_next;
-        }
++ (NSString *)getGateway {
+    NSString *ipString = nil;
+    struct in_addr gatewayaddr;
+    int r = getdefaultgateway(&(gatewayaddr.s_addr));
+    if(r >= 0) {
+        ipString = [NSString stringWithFormat: @"%s",inet_ntoa(gatewayaddr)];
+        NSLog(@"default gateway : %@", ipString );
+    } else {
+        NSLog(@"getdefaultgateway() failed");
     }
     
-    freeifaddrs(interfaces);
-    in_addr_t i = inet_addr([address cStringUsingEncoding:NSUTF8StringEncoding]);
-    in_addr_t* x = &i;
-    unsigned char *s = getdefaultgateway(x);
-    NSString *ip=[NSString stringWithFormat:@"%d.%d.%d.%d",s[0],s[1],s[2],s[3]];
-    free(s);
-    return ip;
+    return ipString;
 }
 
 + (BOOL)matchSinogram:(NSString *)checkString {
