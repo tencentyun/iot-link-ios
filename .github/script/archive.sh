78e433acf4882b021edb7c53bc72c1baf5284db1
eagleychen(陈鹰) <tonychanchen@gmail.com>
2020-09-02 20:51:10 +0800

    打包公版，修改版本号
    Change-Id: Ic801897b90fb5b754c078da72773c8adbf84b1d7

--- .github/script/archive.sh
+++ .github/script/archive.sh
@@ -9,8 +9,8 @@ echo "远程branch"
 
 rb=$GIT_BRANCH_IMAGE_VERSION
 rc=$(git rev-parse --short HEAD)
-rt=$(git describe --tags `git rev-list --tags --max-count=1`)
-#rt=${rtt#*v}
+rtt=$(git describe --tags `git rev-list --tags --max-count=1`)
+rt=${rtt#*v}
 
 echo $rb
 echo $rc
@@ -19,7 +19,7 @@ echo $rt
 if [ $1 == 'Debug' ]; then
     sed -i "" "s/LinkAPP_VERSION.*/LinkAPP_VERSION = $rb+git.$rc/g" Source/LinkApp/Supporting\ Files/LinkAppCommon.xcconfig
 else
-    sed -i "" "s/LinkAPP_VERSION.*/LinkAPP_VERSION = $rt+git.$rc/g" Source/LinkApp/Supporting\ Files/LinkAppCommon.xcconfig
+    sed -i "" "s/LinkAPP_VERSION.*/LinkAPP_VERSION = $rt/g" Source/LinkApp/Supporting\ Files/LinkAppCommon.xcconfig
 fi
 
 cat Source/LinkApp/Supporting\ Files/LinkAppCommon.xcconfig
