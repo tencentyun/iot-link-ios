d8c92ee18cfad7579f5aca2ee7b06c7d32f444cb
ccharlesren <ccharlesren@tencent.com>
2020-10-09 15:57:12 +0800

    创建评测页面控制器
    Change-Id: I34fbd82df7cc68a82179cd93f99aed14ef480c0a

---
+++ Source/LinkApp/Classes/Module/Evaluation/Evaluation/TIoTEvaluationDetailVC.h
@@ -0,0 +1,17 @@
+//
+//  TIoTEvaluationDetailVC.h
+//  LinkApp
+//
+//  Created by ccharlesren on 2020/9/29.
+//  Copyright © 2020 Tencent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TIoTEvaluationDetailVC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Source/LinkApp/Classes/Module/Evaluation/Evaluation/TIoTEvaluationHomeVC.m
@@ -0,0 +1,24 @@
+//
+//  TIoTEvaluationHomeVC.m
+//  LinkApp
+//
+//  Created by ccharlesren on 2020/9/29.
+//  Copyright © 2020 Tencent. All rights reserved.
+//
+
+#import "TIoTEvaluationHomeVC.h"
+
+@interface TIoTEvaluationHomeVC ()
+
+@end
+
+@implementation TIoTEvaluationHomeVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+}
+
+@end
