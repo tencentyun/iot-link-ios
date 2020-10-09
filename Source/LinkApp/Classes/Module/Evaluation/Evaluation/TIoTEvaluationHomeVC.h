d51009a2f6524c0583e80870672fad562d523719
ccharlesren <ccharlesren@tencent.com>
2020-10-09 15:57:12 +0800

    创建评测页面控制器
    Change-Id: I34fbd82df7cc68a82179cd93f99aed14ef480c0a

---
+++ Source/LinkApp/Classes/Module/Evaluation/Evaluation/TIoTEvaluationHomeVC.h
@@ -0,0 +1,17 @@
+//
+//  TIoTEvaluationHomeVC.h
+//  LinkApp
+//
+//  Created by ccharlesren on 2020/9/29.
+//  Copyright © 2020 Tencent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TIoTEvaluationHomeVC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
