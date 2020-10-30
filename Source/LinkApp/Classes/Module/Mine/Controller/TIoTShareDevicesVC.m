c70e1e94c4860130042a3b9aab344a6f594c0c8d
ccharlesren <ccharlesren@tencent.com>
2020-10-30 17:31:12 +0800

    添加分享设备h5自定义面板显示
    Change-Id: Id900df86c3ab6117faed03ced747e3c49416bdb2

--- Source/LinkApp/Classes/Module/Mine/Controller/TIoTShareDevicesVC.m
+++ Source/LinkApp/Classes/Module/Mine/Controller/TIoTShareDevicesVC.m
@@ -9,6 +9,8 @@
 #import "TIoTShareDevicesVC.h"
 #import "TIoTEquipmentTableViewCell.h"
 #import "TIoTPanelVC.h"
+#import "TIoTWebVC.h"
+#import "TIoTAppEnvironment.h"
 
 @interface TIoTShareDevicesVC ()<UITableViewDelegate,UITableViewDataSource>
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
@@ -85,12 +87,78 @@
         [HXYNotice addActivePushPost:devIds];
     }
 
-    TIoTPanelVC *vc = [[TIoTPanelVC alloc] init];
-    vc.title = [NSString stringWithFormat:@"%@",self.dataArr[indexPath.row][@"AliasName"]];
-    vc.productId = self.dataArr[indexPath.row][@"ProductId"];
-    vc.deviceName = [NSString stringWithFormat:@"%@",self.dataArr[indexPath.row][@"DeviceName"]];
-    vc.deviceDic = [self.dataArr[indexPath.row] mutableCopy];
-    [self.navigationController pushViewController:vc animated:YES];
+//    TIoTPanelVC *vc = [[TIoTPanelVC alloc] init];
+//    vc.title = [NSString stringWithFormat:@"%@",self.dataArr[indexPath.row][@"AliasName"]];
+//    vc.productId = self.dataArr[indexPath.row][@"ProductId"];
+//    vc.deviceName = [NSString stringWithFormat:@"%@",self.dataArr[indexPath.row][@"DeviceName"]];
+//    vc.deviceDic = [self.dataArr[indexPath.row] mutableCopy];
+//    [self.navigationController pushViewController:vc animated:YES];
+    
+    
+    
+    NSString *productIDString = self.dataArr[indexPath.row][@"ProductId"] ?:@"";
+    
+    [[TIoTRequestObject shared] post:AppGetProductsConfig Param:@{@"ProductIds":@[productIDString]} success:^(id responseObject) {
+        NSArray *data = responseObject[@"Data"];
+        if (data.count > 0) {
+            NSDictionary *config = [NSString jsonToObject:data[0][@"Config"]];
+            
+            TIoTProductConfigModel *configModel = [TIoTProductConfigModel yy_modelWithJSON:config];
+            if ([configModel.Panel.type isEqualToString:@"h5"]) {
+                
+                //h5自定义面板
+                NSDictionary *deviceDic = [self.dataArr[indexPath.row] copy];
+                NSString *deviceID = deviceDic[@"DeviceId"];
+                NSString *familyId = [TIoTCoreUserManage shared].familyId;
+                NSString *roomID = [TIoTCoreUserManage shared].currentRoomId ? : @"0";
+                NSString *familyType = [NSString stringWithFormat:@"%ld",(long)[TIoTCoreUserManage shared].FamilyType];
+                
+                __weak typeof(self) weadkSelf= self;
+                
+                [MBProgressHUD showLodingNoneEnabledInView:[UIApplication sharedApplication].keyWindow withMessage:@""];
+                [[TIoTRequestObject shared] post:AppGetTokenTicket Param:@{} success:^(id responseObject) {
+
+                    WCLog(@"AppGetTokenTicket responseObject%@", responseObject);
+                    NSString *ticket = responseObject[@"TokenTicket"]?:@"";
+                    NSString *requestID = responseObject[@"RequestId"]?:@"";
+                    NSString *platform = @"iOS";
+                    TIoTWebVC *vc = [TIoTWebVC new];
+                    NSString *bundleId = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleIdentifier"];
+                    NSString *url = [NSString stringWithFormat:@"%@/?deviceId=%@&familyId=%@&appID=%@&roomId=%@&familyType=%@&lid=%@&quid=%@&platform=%@&regionId=%@&ticket=%@&uin=%@", [TIoTCoreAppEnvironment shareEnvironment].deviceDetailH5URL,deviceID,familyId,bundleId,roomID,familyType,requestID,requestID,platform,[TIoTCoreUserManage shared].userRegionId,ticket,TIoTAPPConfig.GlobalDebugUin];
+                    if (deviceDic[@"FromUserID"]) {
+                        NSString *fromUserID = deviceDic[@"FromUserID"];
+                        url = [NSString stringWithFormat:@"%@/?deviceId=%@&familyId=%@&isShareDevice=%@&appID=%@&roomId=%@&familyType=%@&lid=%@&quid=%@&platform=%@&regionId=%@&ticket=%@&uin=%@", [TIoTCoreAppEnvironment shareEnvironment].deviceDetailH5URL,deviceID,familyId,fromUserID,bundleId,roomID,familyType,requestID,requestID,platform,[TIoTCoreUserManage shared].userRegionId,ticket,TIoTAPPConfig.GlobalDebugUin];;
+                    }
+                    
+                    vc.urlPath = url;
+                    vc.needJudgeJump = YES;
+                    vc.needRefresh = YES;
+                    vc.deviceDic = [self.dataArr[indexPath.row] mutableCopy];
+                    [weadkSelf.navigationController pushViewController:vc animated:YES];
+                    [MBProgressHUD dismissInView:weadkSelf.view];
+
+                } failure:^(NSString *reason, NSError *error,NSDictionary *dic) {
+                    [MBProgressHUD dismissInView:weadkSelf.view];
+                }];
+                
+            }else {
+                
+                //标准面板
+                TIoTPanelVC *vc = [[TIoTPanelVC alloc] init];
+                vc.title = [NSString stringWithFormat:@"%@",self.dataArr[indexPath.row][@"AliasName"]];
+                vc.productId = self.dataArr[indexPath.row][@"ProductId"];
+                vc.deviceName = [NSString stringWithFormat:@"%@",self.dataArr[indexPath.row][@"DeviceName"]];
+                vc.deviceDic = [self.dataArr[indexPath.row] mutableCopy];
+//                vc.isOwner = [self.currentFamilyRole integerValue] == 1;
+                vc.configData = config;
+                [self.navigationController pushViewController:vc animated:YES];
+            }
+            
+            
+        }
+    } failure:^(NSString *reason, NSError *error,NSDictionary *dic) {
+        
+    }];
     
 }
 
