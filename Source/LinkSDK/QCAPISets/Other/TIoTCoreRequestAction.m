664a575a4aec990c54f7fecfb4cffe3bf2ff8cab
eagleychen(陈鹰) <tonychanchen@gmail.com>
2020-11-25 20:31:14 +0800

    精简无用UI+串联websocket与被叫UI
    Change-Id: I63cbfbc6c732e8e68709fbd59f70ce95e4b835fd

--- Source/LinkSDK/QCAPISets/Other/TIoTCoreRequestAction.m
+++ Source/LinkSDK/QCAPISets/Other/TIoTCoreRequestAction.m
@@ -133,7 +133,6 @@ NSString *const AppGetSceneAndAutomationLogs = @"AppGetSceneAndAutomationLogs";/
 //MARK: TRTC
 NSString *const AppIotRTCInviteDevice = @"App::IotRTC::InviteDevice";//1.手机请求设备通话
 NSString *const AppIotRTCCallDevice = @"App::IotRTC::CallDevice";//5. 手机请求加入房间参数
-NSString *const AppIotRTCLeavelRoom = @"App::IotRTC::LeavelRoom";//9. 手机离开房间通知
 
 //===============h5
 NSString *const H5HelpCenter = @"help-center";//帮助中心
