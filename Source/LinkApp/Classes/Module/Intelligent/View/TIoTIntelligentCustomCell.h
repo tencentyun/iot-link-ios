e3106c0fe82d415088d0b51fdd62bd395abe2c04
ccharlesren <ccharlesren@tencent.com>
2020-11-27 20:39:54 +0800

    任务项的删除icon需去掉
    文档:https://docs.qq.com/doc/DUHRsSHh1WnRXS215
    
    体验时间：2020.10.21 问题- 5
    
    Change-Id: I24a8c43dcdeb252194ef52b4e43fd9a48ea53fba

--- Source/LinkApp/Classes/Module/Intelligent/View/TIoTIntelligentCustomCell.h
+++ Source/LinkApp/Classes/Module/Intelligent/View/TIoTIntelligentCustomCell.h
@@ -23,7 +23,7 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic, copy) NSString *delayTimeString;
 
 
-@property (nonatomic, copy) DeleteIntelligentItemBlock deleteIntelligentItemBlock; //点击删除功能
+//@property (nonatomic, copy) DeleteIntelligentItemBlock deleteIntelligentItemBlock; //点击删除功能
 @property (nonatomic, assign) BOOL isHideBlankAddView; //自动智能cell 是否显示（默认不显示，条件和任务个数为0时显示）
 @property (nonatomic, strong) NSString *blankAddTipString;   //自动智能添加条件和任务文案提示
 @property (nonatomic, strong) TIoTAutoIntelligentModel *autoIntellModel; //自动智能创建的本地model
