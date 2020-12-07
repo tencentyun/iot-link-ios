d3816873db5f645d926e048916bdcaa0e28dac98
ccharlesren <ccharlesren@tencent.com>
2020-12-08 02:22:14 +0800

    个人详情页面-修改昵称
    文档：https://docs.qq.com/slide/DUGxvTnFjVGFsZExo
    
    问题- 9
    
    Change-Id: Ieacbb7b3c38d0fcb27e24eb266b964b0ff1fc426

---
+++ Source/LinkApp/Classes/AppConfig/AppUIConfig/TIoTModifyNameVC.h
@@ -0,0 +1,27 @@
+//
+//  TIoTModifyNameVC.h
+//  LinkApp
+//
+//  Created by ccharlesren on 2020/12/8.
+//  Copyright © 2020 Tencent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+typedef NS_ENUM(NSInteger, ModifyType) {
+    ModifyTypeNickName,
+    ModifyTypeDeviceName,
+};
+
+typedef void(^ModifyNameBlock)(NSString *name);
+
+@interface TIoTModifyNameVC : UIViewController
+@property (nonatomic, strong)NSString * titleText;
+@property (nonatomic, strong)NSString * defaultText;
+@property (nonatomic, assign)ModifyType modifyType;
+@property (nonatomic, copy) ModifyNameBlock modifyNameBlock;
+@end
+
+NS_ASSUME_NONNULL_END
