4391a91f8e4c94dc7242e59cd89cc69698ae9028
ccharlesren <ccharlesren@tencent.com>
2020-12-17 10:22:28 +0800

    SDKDemo Video 扫码配网
    文档：https://cloud.tencent.com/document/product/1131/42246
    
    Change-Id: I8075a89a59c05f0c0fba646eb5f8eb37fbfca838

---
+++ Source/LinkSDKDemo/Home/Controllers/Device/TIoTVIdeoQRScanDistributionNet.h
@@ -0,0 +1,19 @@
+//
+//  TIoTVIdeoQRScanDistributionNet.h
+//  TIoTLinkKitDemo
+//
+//  Created by ccharlesren on 2020/12/17.
+//  Copyright © 2020 Tencent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+/**
+ Video 扫描二维码配网
+ */
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TIoTVIdeoQRScanDistributionNet : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Source/LinkSDKDemo/Home/Controllers/Device/TIoTVideoSoftApDistributionNetVC.m
@@ -0,0 +1,32 @@
+//
+//  TIoTVideoSoftApDistributionNetVC.m
+//  TIoTLinkKitDemo
+//
+//  Created by ccharlesren on 2020/12/17.
+//  Copyright © 2020 Tencent. All rights reserved.
+//
+
+#import "TIoTVideoSoftApDistributionNetVC.h"
+
+@interface TIoTVideoSoftApDistributionNetVC ()
+
+@end
+
+@implementation TIoTVideoSoftApDistributionNetVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
