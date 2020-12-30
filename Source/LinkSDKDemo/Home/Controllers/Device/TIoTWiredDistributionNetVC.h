ccd0285d1de716b90d7ddfdd67d2d5e9ada53c23
ccharlesren <ccharlesren@tencent.com>
2020-12-31 01:28:00 +0800

    Video 有线配网
    Change-Id: I084b69c7241cb84f717a07a42db716f4a91bcc6f

---
+++ Source/LinkSDKDemo/Home/Controllers/Device/TIoTWiredDistributionNetVC.h
@@ -0,0 +1,17 @@
+//
+//  TIoTWiredDistributionNetVC.h
+//  LinkSDKDemo
+//
+//  Created by ccharlesren on 2020/12/30.
+//  Copyright © 2020 Tencent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TIoTWiredDistributionNetVC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
