561508af95388c95d3c830b2e154020f825167ac
eagleychen(陈鹰) <tonychanchen@gmail.com>
2021-01-29 15:16:33 +0800

    添加video自定义信令
    Change-Id: Id585dd88084e60acc30e4107a68882a2e795846c

--- Source/SDK/LinkVideo/TIoTCoreXP2PBridge.h
+++ Source/SDK/LinkVideo/TIoTCoreXP2PBridge.h
@@ -10,12 +10,19 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+@protocol TIoTCoreXP2PBridgeDelegate <NSObject>
+- (void)getVideoPacket:(uint8_t *)data len:(size_t)len;
+@end
+
+
 @interface TIoTCoreXP2PBridge : NSObject
+@property (nonatomic, weak)id<TIoTCoreXP2PBridgeDelegate> delegate;
 
 + (instancetype)sharedInstance ;
 
 - (void)startAppWith:(NSString *)sec_id sec_key:(NSString *)sec_key pro_id:(NSString *)pro_id dev_name:(NSString *)dev_name ;
 - (NSString *)getUrlForHttpFlv;
+- (void)getCommandRequestWithAsync:(NSString *)cmd timeout:(uint64_t)timeout completion:(void (^ __nullable)(NSString * jsonList))completion;
 
 - (void)sendVoiceToServer;
 - (void)stopVoiceToServer;
--- Source/SDK/LinkVideo/TIoTCoreXP2PBridge.mm
+++ Source/SDK/LinkVideo/TIoTCoreXP2PBridge.mm
@@ -22,6 +22,13 @@ void TTTTLogMessageFunc(int type, char *format) {
     }
 }
 
+void TTTTNativeVideoData(uint8_t *data, size_t len) {
+    id<TIoTCoreXP2PBridgeDelegate> delegate = [TIoTCoreXP2PBridge sharedInstance].delegate;
+    if ([delegate respondsToSelector:@selector(getVideoPacket:len:)]) {
+        [delegate getVideoPacket:data len:len];
+    }
+}
+
 
 @interface TIoTCoreXP2PBridge ()<AWAVCaptureDelegate>
 @end
@@ -73,19 +80,36 @@ void TTTTLogMessageFunc(int type, char *format) {
 
 - (void)startAppWith:(NSString *)sec_id sec_key:(NSString *)sec_key pro_id:(NSString *)pro_id dev_name:(NSString *)dev_name {
 //注册log回调
-//    setNativeCallback(TTTTLogMessageFunc);
-
+    setNativeCallback(TTTTLogMessageFunc);
+    setNativeCallback(TTTTNativeVideoData);
+    
     //1.配置IOT_P2P SDK
     setQcloudApiCred([sec_id UTF8String], [sec_key UTF8String]);
     setDeviceInfo([pro_id UTF8String], [dev_name UTF8String]);
     setXp2pInfoAttributes("_sys_xp2p_info");
-    startServiceWithXp2pInfo("_server_info");
+    startServiceWithXp2pInfo("");
 }
 
 - (NSString *)getUrlForHttpFlv {
     std::string httpflv =  delegateHttpFlv();
     NSLog(@"httpflv---%s",httpflv.c_str());
     return [NSString stringWithCString:httpflv.c_str() encoding:[NSString defaultCStringEncoding]];
+}
+
+- (void)getCommandRequestWithAsync:(NSString *)cmd timeout:(uint64_t)timeout completion:(void (^ __nullable)(NSString * jsonList))completion{
+    
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        
+        char *buf = nullptr;
+        size_t len = 0;
+        getCommandRequestWithSync(cmd.UTF8String, &buf, &len, timeout);
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            if (completion) {
+                completion([NSString stringWithUTF8String:buf]);
+            }
+        });
+    });
 }
 
 - (void)sendVoiceToServer {
