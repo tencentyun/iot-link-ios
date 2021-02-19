2dd647a56dacb7f55293ef788b52a1484fe1fb14
v_vweisun(孙伟) <532125505@qq.com>
2021-02-19 17:58:48 +0800

    【ID861493671】新增开源信息展示入口，开源信息跳转隐私政策英文页面的处理。
    http://tapd.oa.com/20393192/prong/stories/view/1020393192861493671
    
    Change-Id: I1a7c4ea1dee6d52ae0e44551f4025ae002a0fa2d

--- Source/LinkApp/Classes/Module/Mine/Controller/TIoTOpensourceLicenseViewController.m
+++ Source/LinkApp/Classes/Module/Mine/Controller/TIoTOpensourceLicenseViewController.m
@@ -9,9 +9,7 @@
 #import "TIoTOpensourceLicenseViewController.h"
 #import "TIoTOpensourceContentModel.h"
 
-@interface TIoTOpensourceLicenseViewController ()
-
-@property (nonatomic, strong) UIScrollView *scrollView;
+@interface TIoTOpensourceLicenseViewController () <WKNavigationDelegate>
 
 @property (nonatomic, strong) WKWebView *webView;
 
@@ -36,6 +34,7 @@
     configuration.preferences = preferences;
     
     self.webView = [[WKWebView alloc] initWithFrame:CGRectZero configuration:configuration];
+    self.webView.navigationDelegate = self;
     [self.view addSubview:self.webView];
     [self.webView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.leading.trailing.mas_equalTo(0);
@@ -44,7 +43,11 @@
     }];
     
     //拉去静态配置文件（开源软件信息 或其他）
-    [[TIoTRequestObject shared] get:self.urlPath success:^(id responseObject) {
+    [self loadUrl:self.urlPath];
+}
+
+- (void)loadUrl:(NSString *)urlString {
+    [[TIoTRequestObject shared] get:urlString success:^(id responseObject) {
 
         NSDictionary *opensourceDic = (NSDictionary *)responseObject;
         
@@ -54,6 +57,42 @@
     } failure:^(NSString *reason, NSError *error, NSDictionary *dic) {
 
     }];
+}
+
+#pragma mark - WKNavigationDelegate
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler {
+    // 获取完整url并进行UTF-8转码
+        NSString *strRequest = [navigationAction.request.URL.absoluteString stringByRemovingPercentEncoding];
+        if ([strRequest hasPrefix:@"https://imgcache.qq.com/qzone/qzactStatics/qcloud/data"]) {
+            // 拦截点击链接
+            if ([self needHandleURL:strRequest]) {
+                [self loadUrl:strRequest];
+                // 不允许跳转
+                decisionHandler(WKNavigationActionPolicyCancel);
+            } else {
+                // 允许跳转
+                decisionHandler(WKNavigationActionPolicyAllow);
+            }
+        }else {
+            // 允许跳转
+            decisionHandler(WKNavigationActionPolicyAllow);
+            
+        }
+}
+
+- (BOOL)needHandleURL:(NSString *)URL
+{
+    if ([URL hasPrefix:TIoTAPPConfig.opensourceLicenseString]) {
+        // 开源协议信息(英文)
+        return YES;
+    }else if ([URL hasPrefix:TIoTAPPConfig.privacyPolicyEnglishString]) {
+        // 隐私政策(英文)
+        return YES;
+    }else if ([URL hasPrefix:TIoTAPPConfig.serviceAgreementEnglishString]) {
+        // 服务协议(英文)
+        return YES;
+    }
+    return NO;
 }
 
 @end
