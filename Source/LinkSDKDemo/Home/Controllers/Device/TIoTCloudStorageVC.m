1530888fa0d243582c7e2afc809f29fedda080e7
ccharlesren <ccharlesren@tencent.com>
2021-05-11 19:04:04 +0800

    云存录像Demo-滚动播放组件
    Change-Id: I4c0fe75fd87f3e88e77cb27a55b766a913f60a96

--- Source/LinkSDKDemo/Home/Controllers/Device/TIoTCloudStorageVC.m
+++ Source/LinkSDKDemo/Home/Controllers/Device/TIoTCloudStorageVC.m
@@ -8,9 +8,13 @@
 
 #import "TIoTCloudStorageVC.h"
 #import "TIoTCustomCalendar.h"
+#import "NSString+Extension.h"
 
 @interface TIoTCloudStorageVC ()
-
+@property (nonatomic, strong) UISlider *slider;
+@property (nonatomic, strong) NSString *dayDateString; //选择天日期
+@property (nonatomic, strong) UIImageView *imageView;
+@property (nonatomic, strong) UILabel *timeLabel;
 @end
 
 @implementation TIoTCloudStorageVC
@@ -19,20 +23,71 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
+    self.dayDateString = @"";
     [self setupUIViews];
 }
 
 - (void)setupUIViews {
+    
+    [self initializedVideo];
+    
     self.view.backgroundColor = [UIColor whiteColor];
     
+    CGFloat kTopSpace = CGRectGetMaxY(self.imageView.frame) + 10;
+    CGFloat kTimeLabelWidth = 230;
+    
     UIButton *calendarBtn = [UIButton buttonWithType:UIButtonTypeCustom];
-    calendarBtn.frame = CGRectMake(kScreenWidth/2 - 50, kScreenHeight/2 - 50,100, 60);
+    calendarBtn.frame = CGRectMake(20, kTopSpace,100, 40);
     [calendarBtn setTitle:@"日历" forState:UIControlStateNormal];
     calendarBtn.layer.borderColor = [UIColor blueColor].CGColor;
     calendarBtn.layer.borderWidth = 1;
+    calendarBtn.layer.cornerRadius = 10;
     [calendarBtn setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
     [calendarBtn addTarget:self action:@selector(chooseDate) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:calendarBtn];
+    
+    self.timeLabel = [[UILabel alloc]initWithFrame:CGRectMake(kScreenWidth - kTimeLabelWidth - 10, kTopSpace, kTimeLabelWidth, 40)];
+    self.timeLabel.textAlignment = NSTextAlignmentRight;
+    self.timeLabel.text = @"00:00:00";
+    [self.view addSubview:self.timeLabel];
+    
+    
+    CGFloat kTopPadding = 30; //距离日历间距
+    CGFloat kLeftPadding = 50; //左边距
+    CGFloat kItemWith = kScreenWidth/2; //每一天长度
+    CGFloat kScrollContentWidth = kItemWith * 24 + kLeftPadding*2; // 总长度
+    
+    UIScrollView *dateScrollView = [[UIScrollView alloc]initWithFrame:CGRectMake(0, CGRectGetMaxY(calendarBtn.frame)+kTopPadding, kScreenWidth, 50)];
+    [self.view addSubview:dateScrollView];
+    dateScrollView.contentSize = CGSizeMake(kScrollContentWidth, 50);
+    
+    self.slider = [[UISlider alloc]initWithFrame:CGRectMake(kLeftPadding, 0, kScrollContentWidth - kLeftPadding*2, 30)];
+    self.slider.minimumValue = 0;
+    self.slider.maximumValue = 86400; //24*60*60
+    [self.slider addTarget:self action:@selector(sliderDidChangeValue:) forControlEvents:UIControlEventValueChanged];
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(sliderTapped:)];
+    [self.slider addGestureRecognizer:tap];
+    self.slider.continuous = NO;
+    [dateScrollView addSubview:self.slider];
+    
+    for (int i = 0; i < 25; i++) {
+        UIView *lineView = [[UIView alloc]initWithFrame:CGRectMake(i*kItemWith + kLeftPadding, CGRectGetMaxY(self.slider.frame), 1, 20)];
+        lineView.backgroundColor = [UIColor blackColor];
+        [dateScrollView addSubview:lineView];
+        UILabel *timeLabel = [[UILabel alloc]initWithFrame:CGRectMake(CGRectGetMaxX(lineView.frame), CGRectGetMaxY(self.slider.frame), 25, 20)];
+        timeLabel.text = [NSString stringWithFormat:@"%d",i];
+        [dateScrollView addSubview:timeLabel];
+    }
+}
+
+#pragma mark - responsed method
+
+- (void)initializedVideo {
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 64+25, self.view.frame.size.width, self.view.frame.size.width * 9 / 16)];
+    imageView.backgroundColor = [UIColor blackColor];
+    [self.view addSubview:imageView];
+    self.imageView = imageView;
+    self.imageView.userInteractionEnabled = YES;
 }
 
 - (void)chooseDate {
@@ -40,7 +95,43 @@
     [self.view addSubview:view];
     view.selectedDateBlock = ^(NSString *dateString) {
         NSLog(@"%@",dateString);
+        self.dayDateString = dateString;
     };
+}
+
+- (void)sliderDidChangeValue:(UISlider *)sliderControl {
+    
+    NSInteger secondTime = roundf(sliderControl.value);
+    NSString *timeStr = [self getStampDateStringWithSecond:secondTime];
+}
+
+- (void)sliderTapped:(UITapGestureRecognizer *)gesture {
+    
+    CGPoint tapTouchPoint = [gesture locationInView:self.slider];
+    CGFloat value = (self.slider.maximumValue - self.slider.minimumValue) * (tapTouchPoint.x / self.slider.frame.size.width);
+    [self.slider setValue:value animated:YES];
+    
+    NSInteger secondTime = roundf(value);
+    NSString *timeStr = [self getStampDateStringWithSecond:secondTime];
+}
+
+- (NSString *)getStampDateStringWithSecond:(NSInteger )secondTime {
+    NSString *secondString = [NSString getDayFormatTimeFromSecond:[NSString stringWithFormat:@"%ld",secondTime]];
+    NSString *dateStr = [NSString stringWithFormat:@"%@ %@",self.dayDateString,secondString];
+    NSString *stampDate = [NSString getTimeStampWithString:dateStr withFormatter:@"YYYY-MM-dd HH:mm:ss" withTimezone:@""]?:@"";
+    NSLog(@"%@",stampDate);
+    self.timeLabel.text = secondString;
+    return stampDate;
+}
+
+- (CGRect)trackRectForBounds:(CGRect)bounds
+{
+    return CGRectMake(bounds.origin.x, bounds.origin.y, bounds.size.width, 10);
+}
+
+- (CGRect)thumbRectForBounds:(CGRect)bounds trackRect:(CGRect)rect value:(float)value
+{
+    return CGRectMake(bounds.origin.x, bounds.origin.y, bounds.size.width, bounds.size.height);
 }
 
 /*
