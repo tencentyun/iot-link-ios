7fae75c3dae4c68737ff02fa1ac7704ce8e25f0c
eagleychen(陈鹰) <tonychanchen@gmail.com>
2021-06-16 16:52:47 +0800

    rtc呼叫被拒绝处理
    Change-Id: I997c8af0f3548671673520e6948b7166bc96a22e

--- Source/SDK/LinkRTC/TIOTTRTCModel.h
+++ Source/SDK/LinkRTC/TIOTTRTCModel.h
@@ -20,6 +20,8 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic, strong) NSString *_sys_video_call_status;
 @property (nonatomic, strong) NSString *_sys_audio_call_status;
 @property (nonatomic, strong) NSString *_sys_userid;
+@property (nonatomic, strong) NSString *_sys_user_agent; //谁呼叫的
+@property (nonatomic, strong) NSString *_sys_extra_info; //_sys_extra_info: "{ \ "rejectUserId \ ": \ "userb \ "}"消息，判断rejectUserId是否为自己的用户id，，如果为自己的用户id，则退出呼叫通话的流程
 @property (nonatomic, strong) NSString *deviceName;//UI展示的名字
 @property (nonatomic, strong) NSString *username; //用户名
 @end
@@ -29,5 +31,9 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic, strong) NSString *clientToken;
 @property (nonatomic, strong) NSString *timestamp;
 @property (nonatomic, strong) TIOTtrtcPayloadParamModel *params;
+@end
+
+@interface TIOTtrtcRejectModel : NSObject
+@property (nonatomic, strong) NSString *rejectUserId;
 @end
 NS_ASSUME_NONNULL_END
