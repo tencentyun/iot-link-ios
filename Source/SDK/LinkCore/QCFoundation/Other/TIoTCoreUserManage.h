84a6bf3c4f8d7c24c61a2ae362f0a0b8cfd7121b
ccharlesren <ccharlesren@tencent.com>
2021-07-26 15:55:15 +0800

    修改 NVR需求 v2.2.0版本产品反馈问题- ios 登录填充默认信息
    Tapd: http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192865842231?url_cache_key=4f0d71c80d98812e25e7163db84b4e73
    
    设计图: https://codesign.woa.com/s/1Yyg5ZpPkPZ2lKv
    
    反馈列表: https://docs.qq.com/sheet/DQmppVFRwaFlURkpj?tab=BB08J2
    
    Change-Id: I60f7c7bb2914621f289e06b1691944e6ddef449c

--- Source/SDK/LinkCore/QCFoundation/Other/TIoTCoreUserManage.h
+++ Source/SDK/LinkCore/QCFoundation/Other/TIoTCoreUserManage.h
@@ -77,6 +77,13 @@ NS_ASSUME_NONNULL_BEGIN
  */
 @property (nonatomic, copy, nullable) NSString *sys_call_status;
 
+/**
+ SDKDemo
+ */
+@property (nonatomic, copy, nullable) NSString *demoAccessID;
+@property (nonatomic, copy, nullable) NSString *demoAccessToken;
+@property (nonatomic, copy, nullable) NSString *demoProductID;
+
 //保存accessToken 和 有效期
 - (void)saveAccessToken:(NSString *)accessToken expireAt:(NSString *)expireAt;
 
