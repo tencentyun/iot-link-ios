a3c547e19485cad0b85c10799d312572c5ed3681
tonychan <tonychanchen@gmail.com>
2021-09-16 10:45:13 +0800

    添加通过播放器拉流，保存原始数据到Document目录
    Change-Id: I16151cc7e22e439f3d56119603c47a7cf2c4ed8a

--- Source/SDK/LinkVideo/TIoTCoreXP2PBridge.h
+++ Source/SDK/LinkVideo/TIoTCoreXP2PBridge.h
@@ -9,35 +9,81 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @protocol TIoTCoreXP2PBridgeDelegate <NSObject>
+
+/*
+ * 裸流接口使用方式:
+ * 通过 startAvRecvService 和 stopAvRecvService 接口，可以启动和停止裸流传输
+ * 客户端拉取到的裸流数据对应 data 参数
+ */
 - (void)getVideoPacket:(uint8_t *)data len:(size_t)len;
 @end
 
 
 @interface TIoTCoreXP2PBridge : NSObject
 @property (nonatomic, weak)id<TIoTCoreXP2PBridgeDelegate> delegate;
-@property (nonatomic, assign)BOOL writeFile; //是否将数据帧写入文档
-@property (nonatomic, assign)BOOL logEnable; //log 开关，默认打开
 
+/*
+ * 开关是否将数据帧写入 Document 目录的 video.data 文件 （后缀可导出后改动）
+ */
+@property (nonatomic, assign)BOOL writeFile;
+
+/*
+ * 是否打印 SDK Log，默认打开
+ */
+@property (nonatomic, assign)BOOL logEnable;
+
+/*
+ * 获取版本号
+ */
 + (NSString *)getSDKVersion;
 + (instancetype)sharedInstance ;
 
-// 调试SDK功能可以使用此接口，OEM请使用下面的start xp2pinfo, 以防止sec_id ,sec_key泄露
+/*
+ * 调试SDK功能可以使用此接口，OEM请使用下面的start xp2pinfo, 以防止sec_id ,sec_key泄露
+ */
 - (void)startAppWith:(NSString *)sec_id sec_key:(NSString *)sec_key pro_id:(NSString *)pro_id dev_name:(NSString *)dev_name ;
 
-// OEM 版本推荐使用此接口，sec_id, sec_key 传@""即可。 此接口需传从自建服务获取到的 xp2pinfo .
+/*
+ * OEM 版本推荐使用此接口，sec_id, sec_key 传@""即可。 此接口需传从自建服务获取到的 xp2pinfo
+ */
 - (void)startAppWith:(NSString *)sec_id sec_key:(NSString *)sec_key pro_id:(NSString *)pro_id dev_name:(NSString *)dev_name xp2pinfo:(NSString *)xp2pinfo;
 
-
+/*
+ * 使用播放器播放时，需先等待 SDK 初始化完成，ready事件(xp2preconnect 通知)之后，即可获取到 http-url
+ */
 - (NSString *)getUrlForHttpFlv:(NSString *)dev_name;
+
+/*
+ * 与设备信令交互接口
+ * 1.设备端回复 app 的消息没有限制
+ * 2.app 发送给设备的信令，要求不带&符号，信令长度不超过3000个字节
+ *
+ * 事例 cmd 参数（action=inner_define&cmd=get_nvr_list）
+ */
 - (void)getCommandRequestWithAsync:(NSString *)dev_name cmd:(NSString *)cmd timeout:(uint64_t)timeout completion:(void (^ __nullable)(NSString * jsonList))completion;
 
+/*
+ * 开始停止裸流传输接口，通过代理 getVideoPacket 返回裸流数据
+ */
 - (void)startAvRecvService:(NSString *)dev_name cmd:(NSString *)cmd;
 - (void)stopAvRecvService:(NSString *)dev_name;
 
+/*
+ * 语音对讲开始结束接口
+ */
 - (void)sendVoiceToServer:(NSString *)dev_name channel:(NSString *)channel_number;
 - (void)stopVoiceToServer;
 
+/*
+ * 退出 SDK 服务
+ */
 - (void)stopService:(NSString *)dev_name;
+
+/*
+ * 调试接口，录制通过播放器拉取的数据流并行保存到 Document 目录的 video.data 文件
+ * 需提前打开 writeFile 开关
+ */
++ (void)recordstream:(NSString *)dev_name;
 @end
 
 NS_ASSUME_NONNULL_END
