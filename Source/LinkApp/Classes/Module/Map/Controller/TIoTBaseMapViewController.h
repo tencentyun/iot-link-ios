04535c38aa24d675b18b5eb0137100aec012cd6a
ccharlesren <ccharlesren@tencent.com>
2021-12-29 03:23:28 +0800

    【APP】连连APP iOS隐私合规整改问题和解决方案：问题3 不得强迫用户授权
    Tapd: http://tapd.oa.com/NEW_IOT/prong/stories/view/1020393192871147007
    Change-Id: If18eb5e920162ecd5417947bea4656c77072af01

--- Source/LinkApp/Classes/Module/Map/Controller/TIoTBaseMapViewController.h
+++ Source/LinkApp/Classes/Module/Map/Controller/TIoTBaseMapViewController.h
@@ -6,9 +6,20 @@
 #import <QMapKit/QMapKit.h>
 #import <QMapKit/QMSSearchKit.h>
 
+@protocol TIoTBaseMapViewControllerDelegate <NSObject>
+
+/// 用户选择授权后代理
+- (void)agreeLocationAuthorized;
+
+/// 在seting里授权后，返回进入前台后代理
+- (void)enterforegoundAuthorized;
+@end
+
 @interface TIoTBaseMapViewController : UIViewController <QMapViewDelegate>
 
 @property (nonatomic, strong, readonly) QMapView *mapView;
+
+@property (nonatomic, weak) id<TIoTBaseMapViewControllerDelegate>delegate;
 
 #pragma mark - Override
 
